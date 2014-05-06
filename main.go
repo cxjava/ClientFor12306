5005e6d4ab327eb4e18e1408642e52e6c70d84f7
Charles <cxjava@gmail.com>
2014-05-06 22:57:15 +0800

    创建登录窗口

--- main.go
+++ main.go
@@ -1,9 +1,71 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	"github.com/lxn/walk"
+	. "github.com/lxn/walk/declarative"
 )
 
 func main() {
-	fmt.Println("Hello 12306!")
+
+	var mw *walk.MainWindow
+	var acceptPB *walk.PushButton
+
+	if _, err := (MainWindow{
+		AssignTo: &mw,
+		Title:    "Animal Details",
+		MinSize:  Size{100, 50},
+		Layout:   VBox{},
+		Children: []Widget{
+			Composite{
+				Layout: Grid{Columns: 2},
+				Children: []Widget{
+					Label{
+						Text: "用户名:",
+					},
+					LineEdit{
+						Name: "username",
+					},
+
+					Label{
+						Text: "密　码:",
+					},
+					LineEdit{
+						Name:         "password",
+						PasswordMode: true,
+					},
+
+					Label{
+						Text: "验证码:",
+					},
+					LineEdit{
+						Name: "captcha",
+					},
+					VSpacer{
+						ColumnSpan: 1,
+						Size:       8,
+					},
+					LineEdit{
+						// ColumnSpan: 2,
+						Name: "captcha",
+					},
+					VSpacer{
+						ColumnSpan: 1,
+						Size:       8,
+					},
+					PushButton{
+						// ColumnSpan: 2,
+						AssignTo: &acceptPB,
+						Text:     "登陆",
+						OnClicked: func() {
+
+						},
+					},
+				},
+			},
+		},
+	}.Run()); err != nil {
+		log.Fatal(err)
+	}
 }
